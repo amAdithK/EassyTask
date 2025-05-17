8299c76431d4444e615ce88afd52eb3b0b33497e
Adith <adith776020@gmail.com>
2025-05-17 11:01:25 +0530

    Congif changes

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
 export const URLS = {
-  // API_BASE_URL: "https://corptrack-api.suvarnas.com/api",
-  API_BASE_URL: "http://127.0.0.1:8000/api",
+  API_BASE_URL: "https://corptrack-api.suvarnas.com/api",
+  // API_BASE_URL: "http://127.0.0.1:8000/api",
 };
 ``;
